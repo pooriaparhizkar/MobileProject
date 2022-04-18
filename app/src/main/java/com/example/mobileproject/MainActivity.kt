4a74b4675667993eae15bd1bde35ab7266716ef1
Pouorix <pouorix@gmail.com>
2022-04-18 19:42:50 +0430

    Add:Initil project

---
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mobileproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobileproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding :ActivityMainBinding? = null
+    private val binding get()=_binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener{
+            System.out.println("Your message");
+        }
+    }
+}
