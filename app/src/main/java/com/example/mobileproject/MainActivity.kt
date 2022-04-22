8ca4da79870262a6448ce95b70d4fb91458437b5
Pouorix <pouorix@gmail.com>
2022-04-22 18:43:51 +0430

    Add:listener on alternative login method buttons and validation for email and toast for login btn

--- app/src/main/java/com/example/mobileproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileproject/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.mobileproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mobileproject.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private var _binding :ActivityMainBinding? = null
     private val binding get()=_binding!!
 
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         _binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonStart.setOnClickListener{
-            System.out.println("Your message");
+
+       binding.facebook.setOnClickListener {
+           val color = Random()
+           binding.facebook.setBackgroundColor(
+               Color.argb(
+                   255,
+                   color.nextInt(255),
+                   color.nextInt(255),
+                   color.nextInt(255)
+               )
+           )
+
+       }
+        binding.google.setOnClickListener {
+            val color = Random()
+            binding.google.setBackgroundColor(
+                Color.argb(
+                    255,
+                    color.nextInt(255),
+                    color.nextInt(255),
+                    color.nextInt(255)
+                )
+            )
+            Log.d("TAG",binding.emailInput.editText!!.text.toString())
         }
+        binding.nextButton.setOnClickListener {
+            if (!isValidEmail(binding.emailInput.editText!!.text.toString()))
+                binding.emailInput.error="Wrong format"
+            else {
+                binding.emailInput.error=""
+                Toast.makeText(this@MainActivity, "Login successful", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
